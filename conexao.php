d680e6c3b2bace73df0bd37c0a1220a6f4ce1da7
Jucelio <juceliosb@gmail.com>
2021-11-27 03:11:00 -0300

    conexão MariaDB

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php
+
+try{
+
+	// Faz conexão com o banco de daddos
+	$conectar = new PDO("mysql:host=localhost;port=3306;dbname=pdo;", "root", "");
+	echo "conectado com sucesso";
+
+
+} catch (PDOException $e) {
+
+	//Caso ocorra algum erro na conexão com o banco exibe a mensagem
+
+	echo 'Falha ao conectar com o banco de dados:' . $e->getMenssage();
+}
